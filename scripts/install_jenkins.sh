be07ba6929546412061b72c1d8bf28bbbcce0d38
Sainath <sai93@live.in>
2021-12-01 18:28:44 +0530

    commit 1 - 01/12

--- scripts/install_jenkins.sh
+++ scripts/install_jenkins.sh
@@ -10,21 +10,21 @@ sudo apt-get install -y \
     curl \
     gnupg \
     lsb-release
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo \
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+sudo echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-systemctl enable docker
-systemctl start docker
-usermod -aG docker ubuntu
+sudo systemctl enable docker
+sudo systemctl start docker
+sudo usermod -aG docker ubuntu
 
 # run jenkins
-mkdir -p /var/jenkins_home
-chown -R 1000:1000 /var/jenkins_home/
-docker run -p 8080:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_home -d --name jenkins jenkins/jenkins:lts
+sudo mkdir -p /var/jenkins_home
+sudo chown -R 1000:1000 /var/jenkins_home/
+sudo docker run -p 8080:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_home -d --name jenkins jenkins/jenkins:lts
 
 # show endpoint
-echo 'Jenkins installed'
-echo 'You should now be able to access jenkins at: http://'$(curl -s ifconfig.co)':8080'
+sudo echo 'Jenkins installed'
+sudo echo 'You should now be able to access jenkins at: http://'$(curl -s ifconfig.co)':8080'
